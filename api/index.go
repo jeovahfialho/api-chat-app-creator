fe032ede6ce8344ac363b6fcfca4e4e6a68d9e04
Jeovah Simões <jeovahfialho@gmail.com>
2024-10-17 17:08:01 -0300

    index

--- api/index.go
+++ api/index.go
@@ -10,20 +10,26 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func Handler() {
-	r := mux.NewRouter()
-	api.SetupRoutes(r)
+func Handler(w http.ResponseWriter, r *http.Request) {
+	router := mux.NewRouter()
+	api.SetupRoutes(router)
 
 	// Adicione um handler para a rota raiz
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("API is running"))
 	})
 
+	// Ao invés de iniciar um servidor, usamos o router diretamente
+	router.ServeHTTP(w, r)
+}
+
+// init function to set up any necessary configurations
+func init() {
+	// Você pode mover qualquer configuração inicial aqui
+	// Por exemplo, carregar variáveis de ambiente
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-
-	log.Printf("Server starting on port %s", port)
-	log.Fatal(http.ListenAndServe(":"+port, r))
+	log.Printf("API configured to run on port %s", port)
 }
